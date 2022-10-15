80ee2b8c7d183f4e8f9892c60a81e799eec173ab
nanaeka <nanasembiring2019@gmail.com>
2022-10-15 08:14:56 +0700

    first

--- script.js
+++ script.js
@@ -2,29 +2,28 @@ const API_URL =
     "https://api.themoviedb.org/3/discover/movie?api_key=ec2e4fed861b9098a4dfd76fd6d428fc&sort_by=popularity.desc&page=1";
 const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"  
 
-const SEARCH_URL = 
-    "https://api.themoviedb.org/3/search/movie?api_key=ec2e4fed861b9098a4dfd76fd6d428fc&query=${search}&page=1";
+
 const form=document.getElementById('form')
 const search=document.getElementById('search')
-const main=document.getElementById('mainn')
+
+const main=document.getElementById('main')
 
 // Get Movie 
 getMovies(API_URL)
-async function getMovies(url){ 
-const res= await fetch(url)
+async function getMovies(API_URL){ 
+const res= await fetch(API_URL)
 const data= await res.json()
 displayMovie(data.results)
 console.log(data.results);
 }
-function displayMovie('sumbit'){ 
+function displayMovie(movies){ 
     main.innerHTML=''
-    movie.array.forEach(movie=> {
+    movies.forEach((movie)=> {
       const{title,poster_path,vote_average,overview}=movie
-      const moviesElement=document.createElement('')  
+      const moviesElement=document.createElement('div')  
       moviesElement.classList.add('movie')
       moviesElement.innerHTML=`
       <img src="${IMAGE_PATH + poster_path}" alt="${title}" />
-
       <div class'movie-info'>
       <h3>${title}</h3>
       <span class="${getClassesByRating(vote_average)}> ${vote_average}</span>
@@ -48,10 +47,13 @@ function getClassesByRating(rating) {
  }
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
-    const searchValue=search.value
-    if.(searchValue && searchValue !==''){ 
+    searchValue=search.value
+    const SEARCH_URL = 
+    `https://api.themoviedb.org/3/search/movie?api_key=ec2e4fed861b9098a4dfd76fd6d428fc&query=${searchValue}&page=1`;
+    if(searchValue && searchValue !==''){ 
         getMovies(SEARCH_URL+searchValue)
         searchValue=''
     }else{ 
         window.location.reload()
+    }
 })
